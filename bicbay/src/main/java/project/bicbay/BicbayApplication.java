6b7752b703eeb642fdc6f64e1cb7c167199779e9
mylenaverspeelt <mylena49@hotmail.com>
2024-07-23 09:22:02 -0300

    1st commit - hello world controller

---
+++ bicbay/src/main/java/project/bicbay/BicbayApplication.java
@@ -0,0 +1,13 @@
+package project.bicbay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BicbayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BicbayApplication.class, args);
+	}
+
+}
---
+++ bicbay/src/test/java/project/bicbay/BicbayApplicationTests.java
@@ -0,0 +1,13 @@
+package project.bicbay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BicbayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
